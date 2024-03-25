b414d289899d14e938e187a1abb09658ac526a78
sadt <github@sadt.me>
2024-03-25 15:52:47 +0700

    remove plugins

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
@@ -44,7 +43,6 @@ export default defineConfig({
     AutoImport({
       imports: ['vue'],
     }),
-    Components,
   ],
   resolve: {
     alias: {
